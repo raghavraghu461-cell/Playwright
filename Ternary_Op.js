e5513251a25b4f758aaa01db5aa783e9b238960d
M Raghav <raghavraghu461@gmail.com>
2026-05-24 19:06:35 +0530

    save

---
+++ Ternary_Op.js
@@ -0,0 +1,26 @@
+ function goa()
+ {
+     let Raghav_Age=22;
+
+    let result = Raghav_Age > 19
+        ? (Raghav_Age > 21 ? "Drink allowed" : "Not allowed")
+        : "No Goa";
+
+    console.log(result);
+}
+
+goa();
+
+//type of operator
+
+function type()
+{
+    let x="Raghav";
+    console.log(typeof(x));
+}
+
+type();
+
+
+
+
